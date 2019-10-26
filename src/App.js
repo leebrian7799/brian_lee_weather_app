8973c14190e61372f82a2e545dfc7801e8edf383
Brian Lee <leebrian7799@gmail.com>
2019-10-26 14:03:25 -0700

    Spinner added

--- src/App.js
+++ src/App.js
@@ -1,75 +1,100 @@
-import React, {Component} from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { ClipLoader } from "react-spinners";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isLoaded: true,
-      value: '',
-      message: 'e.g. 36.96,-122.02 or London',
-      data: null,
-    }
+      isLoading: false,
+      value: "",
+      message: "e.g. 36.96,-122.02 or London",
+      data: null
+    };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.processText = this.processText.bind(this);
     this.processCoord = this.processCoord.bind(this);
   }
 
-  processText(text){
-    let today = new Date().toJSON().slice(0,10).replace(/-/g,'/');
-    fetch('https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search/?query='+this.state.value)
+  processText(text) {
+    fetch(
+      "https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search/?query=" +
+        this.state.value
+    )
       .then(res => res.json())
-      .then(data =>{
-            switch(data.length){
-              case 0:
-                this.setState({message: 'City not found. Please try another one.'});
-                break;
-              case 1:
-                fetch(`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${data[0].woeid}/${today}`)
-                  .then(resp=>resp.json())
-                  .then(weather=>{
-                      this.setState({data: weather[0]});
-                  });
-                console.log(`https://www.metaweather.com/api/location/${data[0].woeid}/${today}`);
-                break;
-              default:
-                this.setState({message: 'Please narrow down your search.'});
-                break;
-              }
-          }
-      )
+      .then(data => {
+        let message;
+        let weatherData;
+        switch (data.length) {
+          case 0:
+            message = "City not found. Please try another one.";
+            this.setState({ message, data: weatherData, isLoading: false });
+            break;
+          case 1:
+            fetch(
+              `https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${data[0].woeid}/`
+            )
+              .then(resp => resp.json())
+              .then(({ consolidated_weather }) => {
+                if (!!consolidated_weather) {
+                  weatherData = consolidated_weather[0];
+                } else {
+                  message = "No weather data";
+                }
+                this.setState({ message, data: weatherData, isLoading: false });
+              });
+            break;
+          default:
+            message = "Please narrow down your search.";
+            this.setState({ message, data: weatherData, isLoading: false });
+            break;
+        }
+      });
   }
 
-  processCoord(coord){
-      let today = new Date().toJSON().slice(0,10).replace(/-/g,'/');
-      fetch('https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search/?lattlong='+coord)
-      .then(res => res.json())
-      .then(data =>{
-          fetch(`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${data[0].woeid}/${today}`).then(resp=>resp.json())
-          .then(weather=>{
-              this.setState({data: weather[0]});
-          }
-        )
-      }
+  processCoord(coord) {
+    fetch(
+      "https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search/?lattlong=" +
+        coord
     )
+      .then(res => res.json())
+      .then(data => {
+        let message;
+        let weatherData;
+        fetch(
+          `https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${data[0].woeid}/`
+        )
+          .then(resp => resp.json())
+          .then(({ consolidated_weather }) => {
+            if (!!consolidated_weather) {
+              weatherData = consolidated_weather[0];
+            } else {
+              message = "No weather data";
+            }
+            this.setState({ message, data: weatherData, isLoading: false });
+          });
+      });
   }
 
   handleSubmit = event => {
+    event.preventDefault();
+    event.target.reset();
     var letters = /^[A-Za-z\s]+$/;
     var lattlong = /^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?),\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)$/;
 
-    event.preventDefault();
-    event.target.reset();
-    this.setState({message: ''});
+    this.setState({ message: "", isLoading: true });
     if (this.state.value.match(letters)) {
       this.processText(this.state.value);
-    }else if(this.state.value.match(lattlong)) {
+    } else if (this.state.value.match(lattlong)) {
       this.processCoord(this.state.value);
-    }else{
-      this.setState({message: 'Please enter a valid input.'});
+    } else {
+      this.setState({
+        message: "Please enter a valid input.",
+        isLoading: false
+      });
     }
-  }
+  };
 
   handleChange(event) {
     this.setState({
@@ -79,49 +104,67 @@ class App extends Component {
   }
 
   render() {
-    var {
-      isLoaded
-    } = this.state;
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            <h1>Today's Weather</h1>
+            <input
+              type="text"
+              value={this.state.value}
+              placeholder="Search Location"
+              onChange={this.handleChange}
+            />
+          </label>
+          <input type="submit" value="Search" />
+          <div id="message">{this.state.message}</div>
+        </form>
+        <ClipLoader
+          sizeUnit={"px"}
+          size={125}
+          color={"red"}
+          loading={this.state.isLoading}
+        />
+        <Weather data={this.state.data} id="weather">
+          {" "}
+        </Weather>
+      </div>
+    );
+  }
+}
 
-    if (isLoaded) {
-      return (
+const Weather = props => {
+  if (!!props.data) {
+    let imgLink =
+      "https://www.metaweather.com/static/img/weather/" +
+      props.data.weather_state_abbr +
+      ".svg";
+    return (
+      <div className="weather">
+        <img src={imgLink} alt="Icon" id="icon" />
+        <div>{props.data.weather_state_name}</div>
         <div>
-          <form onSubmit = {this.handleSubmit} >
-            <label>
-              <h1>Today's Weather</h1>
-              <input type = "text" value = {this.state.value}
-                placeholder = 'Search Location'
-                onChange = {this.handleChange}/>
-            </label>
-            <input type = "submit" value = "Search" / >
-            <div id='message'>{this.state.message}</div>
-            <Weather data={this.state.data} id='weather'> </Weather>
-          </form>
+          Temperature: {parseInt(props.data.the_temp)}°C /{" "}
+          {parseInt((props.data.the_temp * 9) / 5 + 32)}°F
         </div>
-      )
-    }
-  };
-}
-
-const Weather = (props) => {
-    if (!!props.data){
-      let imgLink = "https://www.metaweather.com/static/img/weather/"+props.data.weather_state_abbr+".svg";
-      console.log(imgLink);
-      return (
-        <div className='weather' >
-            <img src={imgLink} alt='Icon' id='icon'/>
-            <div>{props.data.weather_state_name}</div>
-            <div>Temperature: {parseInt(props.data.the_temp)}°C / {parseInt(props.data.the_temp*9/5+32)}°F</div>
-            <div>High: {parseInt(props.data.max_temp)}°C / {parseInt(props.data.max_temp*9/5+32)}°F</div>
-            <div>Low: {parseInt(props.data.min_temp)}°C / {parseInt(props.data.min_temp*9/5+32)}°F</div>
-            <div>Humidity {parseInt(props.data.humidity)}%</div>
-            <div>Wind: {parseInt(props.data.wind_speed)}mph ({props.data.wind_direction_compass})</div>
+        <div>
+          High: {parseInt(props.data.max_temp)}°C /{" "}
+          {parseInt((props.data.max_temp * 9) / 5 + 32)}°F
         </div>
-    )}else{
-      return (
-        <div></div>
-      )
-    }
-}
+        <div>
+          Low: {parseInt(props.data.min_temp)}°C /{" "}
+          {parseInt((props.data.min_temp * 9) / 5 + 32)}°F
+        </div>
+        <div>Humidity {parseInt(props.data.humidity)}%</div>
+        <div>
+          Wind: {parseInt(props.data.wind_speed)}mph (
+          {props.data.wind_direction_compass})
+        </div>
+      </div>
+    );
+  } else {
+    return <div></div>;
+  }
+};
 
 export default App;
